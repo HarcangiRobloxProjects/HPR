21b516502f21531c70b7054b9fc693ab55b3fd69
Mr Diamond <49098391+MrDiamond64@users.noreply.github.com>
2026-05-04 19:09:46 -0500

    feat: Anticheat section UI render now dynamically displays which RPCs should be analyzed

--- src/ui/sections/AnticheatSection.cs
+++ src/ui/sections/AnticheatSection.cs
@@ -16,18 +16,15 @@ namespace HydraMenu.ui.sections
 
 			Anticheat.CheckSpoofedPlatforms = GUILayout.Toggle(Anticheat.CheckSpoofedPlatforms, "Flag Spoofed Platform Data");
 
-			Anticheat.CheckInvalidCloseDoors = GUILayout.Toggle(Anticheat.CheckInvalidCloseDoors, "Block Invalid CloseDoors RPCs");
-			Anticheat.CheckInvalidCompleteTask = GUILayout.Toggle(Anticheat.CheckInvalidCompleteTask, "Flag Invalid CompleteTask RPCs");
-			Anticheat.CheckInvalidPlayAnimation = GUILayout.Toggle(Anticheat.CheckInvalidPlayAnimation, "Flag Invalid PlayAnimation RPCs");
-			Anticheat.CheckSpoofedLevels = GUILayout.Toggle(Anticheat.CheckSpoofedLevels, "Flag Invalid SetLevel RPCs");
-			Anticheat.CheckInvalidVent = GUILayout.Toggle(Anticheat.CheckInvalidVent, "Flag Invalid EnterVent and ExitVent RPCs");
-			Anticheat.CheckInvalidScan = GUILayout.Toggle(Anticheat.CheckInvalidScan, "Flag Invalid SetScanner RPCs");
-			Anticheat.CheckInvalidSnapTo = GUILayout.Toggle(Anticheat.CheckInvalidSnapTo, "Flag Invalid SnapTo RPCs");
-			Anticheat.CheckInvalidStartCounter = GUILayout.Toggle(Anticheat.CheckInvalidStartCounter, "Flag Invalid SetStartCounter RPCs");
-			Anticheat.CheckInvalidSystemUpdates = GUILayout.Toggle(Anticheat.CheckInvalidSystemUpdates, "Flag Invalid UpdateSystem RPCs");
+			GUILayout.Space(5);
+			GUILayout.Label("RPCs that should be checked by the anticheat:");
+			foreach(var (rpcCall, handler) in Anticheat.RpcHandlers)
+			{
+				handler.Enabled = GUILayout.Toggle(handler.Enabled, $"{rpcCall}");
+			}
 
 			GUILayout.Space(5);
-			GUILayout.Label("When a cheating is detected:");
+			GUILayout.Label("When a cheater is detected:");
 			Anticheat.DiscardRPC = GUILayout.Toggle(Anticheat.DiscardRPC, "Discard RPC");
 
 			GUILayout.BeginHorizontal();
